a04be18135f6cf62ee10ac15608371e61819f4c1
sutter-dave <sutter@intransix.com>
2021-03-10 16:57:14 -0800

    added a choice option for form expressions

--- src/configurablepanel/ConfigurablePanelUtil.js
+++ src/configurablepanel/ConfigurablePanelUtil.js
@@ -15,46 +15,71 @@ export function getFormResultFunctionBody(formValue,formMeta) {
 }
 
 /** This loads a value to an assignee, as part of the form result function body. */
-function _loadEntry(assigneeName,value,meta,functionLines) {
+function _loadEntry(assigneeName,value,containerValue,meta,functionLines) {
     if((value === undefined)||(value === null)||((meta)&&(meta.excludeValue !== undefined)&&(meta.excludeValue === value))) {
         //value excluded
         //handle value === undefined!!
         return false;
     }
-        
-    //add value
+
+    //get the expression type
+    let expressionType;
     if((meta)&&(meta.expression)) {
-        //expression
-        switch(meta.expression) {
-            case "simple":
-                return _loadSimpleExpressionEntry(assigneeName,value,functionLines);
-                
-            case "object":
-                functionLines.push(assigneeName + "= {};");
-                _loadPanelLines(assigneeName,value,meta.childMeta,functionLines);
-                //for now we always add the base object, even if empty
-                return true;
-                
-            case "array": 
-                functionLines.push(assigneeName + "= []");
-                if(meta.childMeta) {
-                    _loadMultiTypedArrayLines(assigneeName,value,meta.childMeta,functionLines);
-                }
-                else if(meta.entryMeta) {
-                    _loadSingleTypedArrayLines(assigneeName,value,meta.entryMeta,functionLines);
-                }
-                //for now we always add the base array, even if empty
-                return true;
-                
-            default:
-                console.error("Expression type not supported: " + meta.expression);
-                return false;
+        if(meta.expression == "choice") {
+            expressionType = _getExpressionChoiceType(assigneeName,containerValue,meta);
+        }
+        else {
+            expressionType = meta.expression;
         }
     }
     else {
-        //plain value, not an expression
-        return _loadSimpleValueEntry(assigneeName,value,functionLines);
+        expressionType = "value";
     }
+        
+
+    //get the value or expression
+    switch(expressionType) {
+        case "value": 
+            //plain value, not an expression
+            return _loadSimpleValueEntry(assigneeName,value,functionLines);
+
+        case "simple":
+            //this is an expression
+            return _loadSimpleExpressionEntry(assigneeName,value,functionLines);
+            
+        case "object":
+            //this is an object containing a possible expression
+            functionLines.push(assigneeName + "= {};");
+            _loadPanelLines(assigneeName,value,meta.childMeta,functionLines);
+            //for now we always add the base object, even if empty
+            return true;
+            
+        case "array": 
+            //this is an array containing a possible expression
+            functionLines.push(assigneeName + "= []");
+            if(meta.childMeta) {
+                _loadMultiTypedArrayLines(assigneeName,value,meta.childMeta,functionLines);
+            }
+            else if(meta.entryMeta) {
+                _loadSingleTypedArrayLines(assigneeName,value,meta.entryMeta,functionLines);
+            }
+            //for now we always add the base array, even if empty
+            return true;
+            
+        default:
+            throw new Error("Expression type not supported for " + assigneeName + ": " + expressionType);
+    }
+}
+
+/** This gets the expression type where the type is listed as choice. */
+function _getExpressionChoiceType(assigneeName,containerValue,meta) {
+    if(!containerValue) throw new Expression("Error in choice expression for " + assigneeName + ". Not in a valid parent object.")
+    if(!meta.expressionChoiceKey) throw new Error("Meta expressionChoiceKey entry missing for " + assigneeName);
+
+    let expressionType = containerValue[meta.expressionChoiceKey];
+    if(!expressionType) throw new Error("Expression choice not found for key for " + assigneeName + ": " + meta.expressionChoiceKey);
+    
+    return expressionType;
 }
 
 /** This loads a value to an assignee for a simple value, as part of the form result function body. */
@@ -80,7 +105,7 @@ function _loadPanelLines(parentObjectName,panelValue,panelMeta,functionLines) {
         let meta = panelMeta[key];
         let value = panelValue[key];
         let assigneeName = parentObjectName + "[" + JSON.stringify(key) + "]";
-        _loadEntry(assigneeName,value,meta,functionLines);
+        _loadEntry(assigneeName,value,panelValue,meta,functionLines);
     }
 }
 
@@ -93,7 +118,7 @@ function _loadMultiTypedArrayLines(assigneeName,value,metaMap,functionLines) {
         let entryValue = keyedEntry.value;
         let entryMeta = metaMap[key];
         let entryAssigneeName = assigneeName + "[" + insertIndex + "]";
-        let lineAdded = _loadEntry(entryAssigneeName,entryValue,entryMeta,functionLines);
+        let lineAdded = _loadEntry(entryAssigneeName,entryValue,null,entryMeta,functionLines);
         if(lineAdded) {
             insertIndex++;
             linesAdded = true;
@@ -108,7 +133,7 @@ function _loadSingleTypedArrayLines(assigneeName,value,entryMeta,functionLines) 
     let linesAdded = false;
     value.forEach( (entryValue) => {
         let entryAssigneeName = assigneeName + "[" + insertIndex + "]";
-        let lineAdded = _loadEntry(entryAssigneeName,entryValue,entryMeta,functionLines);
+        let lineAdded = _loadEntry(entryAssigneeName,entryValue,null,entryMeta,functionLines);
         if(lineAdded) {
             insertIndex++;
             linesAdded = true;
